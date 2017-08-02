43b7f62e1ad638ffce325cbabf3ae81ff9e3ee94
Leif <870510789@qq.com>
2017-08-02 22:52:18 +0800

    2

--- test14.py
+++ test14.py
@@ -1,53 +1,53 @@
-# --*-- coding:utf-8 --*--
-# author: leif
-# 淘宝商品信息抓取
-import re
+#--*-- coding:utf-8 --*--
+#抓取淘宝商品信息
+# leif ```  站点 www.aotur.com
 import requests
+import re
 
-def getHtml(url):
+def getHTMLText(url):
     try:
-        r = requests.get(url,timeout=30)
+        r = requests.get(url, timeout=30)
         r.raise_for_status()
-        r.encoding=r.apparent_encoding
+        r.encoding = r.apparent_encoding
         return r.text
     except:
-        return " "
+        return ""
 
-def parsePage(ilt,html):
+
+def parsePage(ilt, html):
     try:
-        plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"',html)
-        tlt = re.findall(r'\"raw_title\"\:\".*?"',html)
-        num = re.findall(r'\"view_sales\"\:\"[\d\.]*人付款\"',html)
+        plt = re.findall(r'\"view_price\"\:\"[\d\.]*\"', html)
+        tlt = re.findall(r'\"raw_title\"\:\".*?\"', html)
         for i in range(len(plt)):
-            price = eval(plt[i].split(':')[1])     # eval 去除双引号，  split 用 ： 作为分割，取后半部
+            price = eval(plt[i].split(':')[1])
             title = eval(tlt[i].split(':')[1])
-            number = eval(num[i].split(':')[1])
-            ilt.append([price,number,title])
+            ilt.append([price, title])
     except:
         print("")
 
-def printInfo(ilt):
-    tplp = "{:4}\t{:8}\t{:8}\t{:18}"
-    print(tplp.format("序号","价格","付款数","商品名称"))
+
+def printGoodsList(ilt):
+    tplt = "{:4}\t{:8}\t{:16}"
+    print(tplt.format("序号", "价格", "商品名称"))
     count = 0
     for g in ilt:
         count = count + 1
-        print(tplp.format(count, g[0], g[1], g[2]))
+        print(tplt.format(count, g[0], g[1]))
+
 
 def main():
-    goods="裙子"
-    depth=2
-    start_url='https://s.taobao.com/search?q=' + goods
+    goods = '裙子'
+    depth = 3
+    start_url = 'https://s.taobao.com/search?q=' + goods
     infoList = []
     for i in range(depth):
         try:
-            url=start_url + '&s=' + str(44*i)
-            html = getHtml(url)
-            parsePage(infoList,html)
+            url = start_url + '&s=' + str(44 * i)
+            html = getHTMLText(url)
+            parsePage(infoList, html)
         except:
             continue
-    printInfo(infoList)
+    printGoodsList(infoList)
+
 
 main()
-
-
